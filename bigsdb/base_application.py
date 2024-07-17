c631b2e3209d0e7812fec8f5a2ab71d77e273e39
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2024-07-17 21:32:59 +0100

    Read config.xml and system.overrides. Set system dict.

--- bigsdb/base_application.py
+++ bigsdb/base_application.py
@@ -11,7 +11,7 @@
 #
 # BIGSdb Python Toolkit is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
@@ -19,27 +19,36 @@
 # <http://www.gnu.org/licenses/>.
 
 import configparser
+from pathlib import Path
 import bigsdb.utils
+import bigsdb.xml_parser as xml_parser
 
 CONFIG_DIR = '/etc/bigsdb'
 DBASE_CONFIG_DIR = '/etc/bigsdb/dbases'
 
+
 class Base_Application(object):
     
-    def __init__(self, database=None, config_dir=CONFIG_DIR, dbase_config_dir=DBASE_CONFIG_DIR, testing = False):
+    def __init__(self, database=None, config_dir=CONFIG_DIR,
+                 dbase_config_dir=DBASE_CONFIG_DIR, testing=False):
         self.config_dir = config_dir
         self.dbase_config_dir = dbase_config_dir
         if testing:
             return
         if database == None:
             raise ValueError('No database parameter passed.')
+        self.instance = database
         self.config = self.__read_config_file()
-        print(self.config)
+        self.__read_dbase_config_file()
+        
+        self.__set_system_overrides()
+#        print(self.system)
+#        print(self.config)
         
     def __read_config_file(self, filename=None):
         filename = filename or f"{self.config_dir}/bigsdb.conf"
         with open(filename, 'r') as f:
-             ini_data =  '[General]\n' + f.read()
+             ini_data = '[General]\n' + f.read()
         config = configparser.ConfigParser()
         config.read_string(ini_data)
         dict = {}
@@ -54,4 +63,31 @@ class Base_Application(object):
             dict[key] = value
         return dict
     
-
+    def __read_dbase_config_file(self, filename=None):
+        filename = filename or f'{self.dbase_config_dir}/{self.instance}/config.xml'
+        if Path(filename).is_file():
+            self.parser = xml_parser.XML_Parser()
+            self.parser.parse(filename)
+            self.system = self.parser.get_system()
+        else:
+            raise ValueError(f'Database config file {filename} does not exist.')
+    
+    def __set_system_overrides(self, filename=None):
+        filename = filename or f'{self.dbase_config_dir}/{self.instance}/system.overrides'
+        if not Path(filename).is_file():
+            return
+        with open(filename, 'r') as f:
+             ini_data = '[General]\n' + f.read()
+        config = configparser.ConfigParser()
+        config.read_string(ini_data)
+        for key in config['General']:
+            value = config['General'][key]
+            value = value.strip('"')
+            if (bigsdb.utils.is_integer(value)):
+                value = int(value)
+            elif (bigsdb.utils.is_float(value)):
+                value = float(value)
+            elif (bigsdb.utils.is_date(value)):
+                value = date(value) 
+            print (f'{key}: {value}')         
+            self.system[key] = value
